343801ff203c3b8d94a64d19a139110e82c0d4b5
Cameron Carroll <ckckarroll4@gmail.com>
2013-07-30 02:38:37 -0700

    added constants for configuration, updated readme

---
+++ readme.md
@@ -0,0 +1,13 @@
+cameron's ruby scripts
+----------------------
+
+screenshot.rb:
+==============
+Created: July 2013
+Last Updated: July 2013 (1.2)
+Purpose: takes a screenshot using scrot after argv[0] number of seconds
+Dependencies: scrot
+Configuration & Installation:
+    a) replace SCREENSHOT_FOLDER with your own home folder
+    b) select a reasonable MAX_DELAY_TIME
+    c) symlink and chmod +x screenshot.rb into your favorite bin folder
--- screenshot.rb
+++ screenshot.rb
@@ -1,37 +1,45 @@
 #!/usr/bin/ruby
 # script file: screenshot.rb
 # author: cameron carroll; created july 2013
-# purpose: takes a screenshot using imagemagick import after argv[0] number of seconds
-# dependencies: imagemagick
+# purpose: takes a screenshot using scrot after argv[0] number of seconds
+# dependencies: scrot
+# configuration & installation:
+#                a) replace SCREENSHOT_FOLDER with your own home folder
+#                b) select a reasonable MAX_DELAY_TIME
+#                c) symlink and chmod +x screenshot.rb into your favorite bin folder
 
-VERSION = '1.1'
+VERSION = '1.2'
+SCREENSHOT_FOLDER = '/home/cameron/images/screenshots'
+MAX_DELAY_TIME = 20
 
 def shoot()
   time = Time.new
   puts "Taking screenshot at #{time}"
   time = time.to_s[0...-6].gsub(' ', '_').gsub('-', '').gsub(':', '')
-  `scrot ~/images/screenshots/#{time}.png`
+  `scrot #{SCREENSHOT_FOLDER}/#{time}.png`
 end
 
 def delay_shoot(delay_time)
   puts "Waiting #{delay_time} seconds before taking screenshot"
-  sleep(delay_time.to_i) if delay_time.to_i < 60
+  sleep(delay_time.to_i) if delay_time.to_i < MAX_DELAY_TIME
   shoot()
 end
 
 def main
   puts "screenshot.rb #{VERSION}"
-  delay = ARGV[0] if ARGV[0]
+  delay &&= ARGV[0] # assign if it exists
+
   if delay && delay.numeric?
     delay_shoot(ARGV[0])
   elsif delay && !delay.numeric?
     input = delay # rarely we will call with a proper string, so now we'll just call it input
     if input == 'clear'
-      `rm /home/cameron/images/screenshots/* -rf`
+      `rm #{SCREENSHOT_FOLDER}/* -rf`
     else
       puts 'unknown input'
     end
   end
+
 end
 
 class String
