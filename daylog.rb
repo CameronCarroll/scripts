da02ff99f4d8cbc9794bbccd6f6566e621fe9092
Cameron Carroll <ckcarroll4@gmail.com>
2013-12-17 14:06:31 -0800

    (daylog.rb) code cleanup (cosmetic)

--- daylog.rb
+++ daylog.rb
@@ -1,31 +1,25 @@
 #!/usr/bin/ruby
-# script file: daylog.rb
-# author: cameron carroll 2013
-# purpose:
-#     Accepts one-off statements and adds them to the appropriate write_everyday file
+# Script File: daylog.rb
+# Author: Cameron Carroll 2013
+# Purpose: Accepts one-off statements and adds them to the appropriate write_everyday file
 
 DOC_DIRECTORY = '/home/cameron/docs/write_everyday/'
 
-def main
-  month = Time.new.strftime("%B")
-  day = Time.new.day
-  filestring = DOC_DIRECTORY + "#{month}"
+month = Time.new.strftime("%B")
+day = Time.new.day
+file = DOC_DIRECTORY + month
 
-  inputstring = ' '
-  ARGV.each do |arg|
-    inputstring += arg + ' '
-  end
-
-  unless File.exists?(filestring)
-    File.new(filestring, 'w')
-  end
-
-  File.open(filestring, 'a') do |file|
-    date_banner = Time.new.strftime("On %m/%d/%Y, at %I:%M%p:")
-    file << date_banner << "\n"
-    file << inputstring << "\n" << "\n"
-  end
-
+input = ' '
+ARGV.each do |arg|
+  input += arg + ' '
 end
 
-main
+unless File.exists?(file)
+  File.new(file, 'w')
+end
+
+File.open(file, 'a') do |file|
+  date_banner = Time.new.strftime("On %m/%d/%Y, at %I:%M%p:")
+  file << date_banner << "\n"
+  file << input << "\n" << "\n"
+end
