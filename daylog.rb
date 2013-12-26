23e5e7138046925450a15bdb8cbd241a6d0faa73
Cameron Carroll <ckcarroll4@gmail.com>
2013-12-26 03:13:15 -0800

    Added git-commit-style input for daylog.rb

--- daylog.rb
+++ daylog.rb
@@ -2,17 +2,35 @@
 # Script File: daylog.rb
 # Author: Cameron Carroll 2013
 # Purpose: Accepts one-off statements and adds them to the appropriate write_everyday file
-
+# Version: 1.2
+if ARGV.length > 0
+  input = ' '
+  ARGV.each do |arg|
+    input += arg + ' '
+  end
+else
+  TEMPFILE = 'daylog.tmp'
+  system("vim #{TEMPFILE}")
+  input = ""
+  begin
+    File.open(TEMPFILE, 'r') do |tempfile|
+      while (line = tempfile.gets)
+        input << line
+      end
+    end
+  rescue => err
+    puts err
+    err
+  end
+  File.delete TEMPFILE
+end
 DOC_DIRECTORY = '/home/cameron/docs/write_everyday/'
+Dir.mkdir(DOC_DIRECTORY) unless Dir.exists?(DOC_DIRECTORY)
 
 month = Time.new.strftime("%B")
 day = Time.new.day
 file = DOC_DIRECTORY + month
 
-input = ' '
-ARGV.each do |arg|
-  input += arg + ' '
-end
 
 unless File.exists?(file)
   File.new(file, 'w')
