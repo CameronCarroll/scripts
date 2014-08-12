7807896a3b0265b2c6995f75816bcd22248099a8
Cameron Carroll <ckcarroll4@gmail.com>
2014-08-12 10:12:23 -0700

    Add compress_pdf.rb

--- compress_pdf.rb
+++ compress_pdf.rb
@@ -2,11 +2,15 @@
 #
 # Script File: compress_pdf.rb
 # Purpose: Feeds filename into ghostscript to compress it.
+
+require 'fileutils'
+
 if ARGV.first
   file = ARGV.first
   puts file
   if file && File.exists?(file)
     puts `gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=#{file}c #{file}`
+    FileUtils.mv("#{file}c", "#{file}")
   end
 else
   Dir["*.pdf"].each do |pdf_file|
